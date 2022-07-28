b9faa77d5c4632a79998539834f161fb2ac89975
Nilesh Porwal <aashul573@gmail.com>
2022-07-28 23:53:00 +0530

    third

--- 01knapsack.py
+++ 01knapsack.py
@@ -8,7 +8,22 @@ find out the maximum value subset of val[] such that
  sum of the weights of this subset is smaller than or equal to W.
   You cannot break an item, either pick the complete item or dont pick it (0-1 property)."""
 #nilesh
-#shubham
+#Top-Down Dp
+def knapSack(self,W, wt, val, n):
+        dp=[[0 for i in range(W+1)]for j in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,W+1):
+                if wt[i-1]<=j:
+                    dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j])
+                else:
+                    dp[i][j]=dp[i-1][j]
+                
+        return dp[n][W]
+
+
+
+
+
 def knapsack(wt,val,w,n):
     if n==0 or w==0:
         return 0
